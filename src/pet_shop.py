24437b127384675ede4a8f732c99229d1058fa60
svvishjs <andreww92@live.com>
2020-09-12 09:16:08 +0100

    get_total_cash function added

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,4 +4,10 @@ def get_pet_shop_name(dictionary):
     name = dictionary["name"]
     return name
 
-    
+
+def get_total_cash(dictionary):
+    total_cash = dictionary["admin"]["total_cash"]
+    return total_cash
+
+
+def  
