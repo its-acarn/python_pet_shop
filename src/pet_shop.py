2b1c1fb35d00e47ecef932ca23aa6bf76d594606
svvishjs <andreww92@live.com>
2020-09-12 09:42:56 +0100

    add_or_remove_cash function

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,13 +1,18 @@
 # WRITE YOUR FUNCTIONS HERE
 
-def get_pet_shop_name(dictionary):
-    name = dictionary["name"]
+def get_pet_shop_name(dict):
+    name = dict["name"]
     return name
 
 
-def get_total_cash(dictionary):
-    total_cash = dictionary["admin"]["total_cash"]
+def get_total_cash(dict):
+    total_cash = dict["admin"]["total_cash"]
     return total_cash
 
 
-def  
+def add_or_remove_cash(dict, cash_difference):
+    dict["admin"]["total_cash"] += cash_difference
+    return dict["admin"]["total_cash"]
+
+# def gets_pets_sold():
+
