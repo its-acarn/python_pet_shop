468f56bb9dbd95af1f4ab48e9c9412745e913903
svvishjs <andreww92@live.com>
2020-09-14 10:43:58 +0100

    amended dict variable to be called pets_dict

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,28 +1,28 @@
 # WRITE YOUR FUNCTIONS HERE
 
-def get_pet_shop_name(dict):
-    name = dict["name"]
+def get_pet_shop_name(pets_dict):
+    name = pets_dict["name"]
     return name
 
 
-def get_total_cash(dict):
-    total_cash = dict["admin"]["total_cash"]
+def get_total_cash(pets_dict):
+    total_cash = pets_dict["admin"]["total_cash"]
     return total_cash
 
 
-def add_or_remove_cash(dict, cash_difference):
-    dict["admin"]["total_cash"] += cash_difference
+def add_or_remove_cash(pets_dict, cash_difference):
+    pets_dict["admin"]["total_cash"] += cash_difference
     # return dict["admin"]["total_cash"] #no return needed here!
 
 
-def get_pets_sold(dict):
-    no_pets_sold = dict["admin"]["pets_sold"]
+def get_pets_sold(pets_dict):
+    no_pets_sold = pets_dict["admin"]["pets_sold"]
     return no_pets_sold
 
 
-def increase_pets_sold(dict, num_of_pets_change):
-    dict["admin"]["pets_sold"] += num_of_pets_change
-    return dict["admin"]["pets_sold"]
+def increase_pets_sold(pets_dict, num_of_pets_change):
+    pets_dict["admin"]["pets_sold"] += num_of_pets_change
+    return pets_dict["admin"]["pets_sold"]
 
 
 def get_stock_count(pets_dict):
